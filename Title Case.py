05ccaec3544fe929156795645819738e4d512868
Mphakian <princemphaka3@gmail.com>
2025-04-30 21:33:53 +0200

    ...

--- Title Case.py
+++ Title Case.py
@@ -13,3 +13,5 @@ First argument (required): the original string to be converted.
 Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string. 
 The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
 '''
+def title_case(title) -> str:
+    ...
