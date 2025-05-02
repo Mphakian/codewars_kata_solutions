641754ab8e4380a26b30097a71a9b4fab2557189
Mphakian <princemphaka3@gmail.com>
2025-05-02 11:06:14 +0200

    Next task: figure out how to redraw the map, after a move

--- Labyrinth Game/AI on Labyrinth Game.py
+++ Labyrinth Game/AI on Labyrinth Game.py
@@ -23,15 +23,17 @@ limit may be x3.
 
 
 class WormAI:
-    def __init__(self) -> None:
-        ...
+    def __init__(self, game_map : str) -> None:
+        self.game_map = game_map
+        self.hist_pos = []
+
     def on_state(self, m, position) -> None: # m[y][x], position=(x,y)
         ...
         
-    def read_map(self, map : str) -> list:
-        with open(f'.\Labyrinth Game\{map}', encoding='utf-8') as f:
+    def read_map(self) -> list:
+        with open(f'.\Labyrinth Game\{self.game_map}', encoding='utf-8') as f:
             content = f.read()
-        return content.splitlines()
+        return content.splitlines()                                                    #The contents (strings) in the mappped_list are not be replaced.... If I remember well
     
     def starting_pos(self, mapped_list : list) -> tuple:
         for i in range(len(mapped_list)):
@@ -39,11 +41,31 @@ class WormAI:
                 if mapped_list[i][j] == '0':
                     return i,j
         
-    def do_move(self) -> str: #return single character of WASD or 8426
-        ...
+    def do_move(self, move : str | int) -> tuple: #return single character of WASD or 8426
+        current_pos = self.starting_pos(self.read_map())
+        self.hist_pos.append(current_pos)
+        #REMINDER: current_pos = (height, width) of the map
+        #For W/8 -> 0 must be moved 1 postion up (1 height up)
+            #Must check if it does not bump into walls..........this part will be solved when I implement the 3x3 scanned map
+            #Maybe check if it the next current_pos is == 'I' which would mean it has solved the map
+        if move == 'W' or move == 8 or move == 'w':
+            #The desired output is current_pos = (height -1, width)
+            current_pos = (current_pos[0] - 1, current_pos[1])
+            self.hist_pos.append(current_pos)
+
+            #Redraw the map
+            
+
+        #For S/2 -> 0 must be moved 1 postion down (1 height down)
+        #For A/4 -> 0 must be moved 1 postion left (1 height left)
+        #For D/6 -> 0 must be moved 1 postion right (1 height right)
+
+        return current_pos
         
-test : WormAI = WormAI()
+if __name__ == '__main__':
 
-mapped_list = test.read_map('map_1.txt')
+    test : WormAI = WormAI('map_1.txt')
 
-print(test.starting_pos(mapped_list))
+    
+
+
